6bcd95788ab3854445b16da69af99654995381f1
Steven Myers <stelmyer@umail.iu.edu>
2016-10-17 19:29:01 -0400

    Changed the windows ascii art to better resemble windows logo

--- screenfetch.ps1
+++ screenfetch.ps1
@@ -8,7 +8,7 @@ $Horizontal = (gwmi -Class Win32_VideoController).CurrentHorizontalResolution | 
 $Vertical = (gwmi -Class Win32_VideoController).CurrentVerticalResolution | select -f 1;
 
 ## Uptime Information
-$uptime = ((gwmi Win32_OperatingSystem).ConvertToDateTime((gwmi Win32_OperatingSystem).LocalDateTime) - 
+$uptime = ((gwmi Win32_OperatingSystem).ConvertToDateTime((gwmi Win32_OperatingSystem).LocalDateTime) -
            (gwmi Win32_OperatingSystem).ConvertToDateTime((gwmi Win32_OperatingSystem).LastBootUpTime));
 
 ## Disk Information
@@ -34,7 +34,7 @@ $Kernel = (gwmi Win32_OperatingSystem).Version;
 $Motherboard = Get-CimInstance Win32_BaseBoard | Select-Object Manufacturer, Product;
 $CPU = (((gwmi Win32_Processor).Name) -replace '\s+', ' ');
 $GPU = (gwmi Win32_DisplayConfiguration).DeviceName;
-$FreeRam = ([math]::Truncate((gwmi Win32_OperatingSystem).FreePhysicalMemory / 1KB)); 
+$FreeRam = ([math]::Truncate((gwmi Win32_OperatingSystem).FreePhysicalMemory / 1KB));
 $TotalRam = ([math]::Truncate((gwmi Win32_ComputerSystem).TotalPhysicalMemory / 1MB));
 $RamPercent = ($FreeRam / $TotalRam) * 100;
 $RamPercent = "{0:N0}" -f $RamPercent;
@@ -43,63 +43,64 @@ $RamPercent = "{0:N0}" -f $RamPercent;
 ####### Printing Output #########
 
 # Line 1 - HostName
-Write-Host "                         ....::::       " -f Cyan -NoNewline;
-Write-Host $Username -f red -nonewline; 
-Write-Host "@" -f gray -nonewline; 
+Write-Host `n`n
+Write-Host "                            .;;;;;    " -f Cyan -NoNewline;
+Write-Host $Username -f red -nonewline;
+Write-Host "@" -f gray -nonewline;
 Write-Host $Machine -f red;
 
 # Line 2 - OS
-Write-Host "                 ....::::::::::::       " -f Cyan -NoNewline;
+Write-Host "                     .;;;;;;;;;;;;    " -f Cyan -NoNewline;
 Write-Host "OS: " -f Red -NoNewline;
 Write-Host $OS $BitVer;
 
 # Line 3 - Kernel
-Write-Host "        ....:::: ::::::::::::::::       " -f Cyan -NoNewline;
+Write-Host "              .;; ;;;;;;;;;;;;;;;;    " -f Cyan -NoNewline;
 Write-Host "Kernel: " -f Red -nonewline;
 Write-Host $Kernel;
 
 # Line 4 - Uptime
-Write-Host "....:::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
+Write-Host "       ;;;;;;;;;; ;;;;;;;;;;;;;;;;    " -f Cyan -NoNewline;
 Write-Host "Uptime: " -f Red  -nonewline;
 Write-Host $uptime.Days"d " $uptime.Hours"h " $uptime.Minutes"m " $uptime.Seconds"s " -separator "";
 
 # Line 5 - Motherboard
-Write-Host ":::::::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
-Write-Host "Motherboard: " -f Red -nonewline; 
+Write-Host "      ;;;;;;;;;;; ;;;;;;;;;;;;;;;;    " -f Cyan -NoNewline;
+Write-Host "Motherboard: " -f Red -nonewline;
 Write-Host $Motherboard.Product;
 
 # Line 6 - Shell (Hardcoded since it is unlikely anybody can run this without powershell)
-Write-Host ":::::::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
-Write-Host "Shell: " -f Red -nonewline; 
+Write-Host "      ;;;;;;;;;;; ;;;;;;;;;;;;;;;;    " -f Cyan -NoNewline;
+Write-Host "Shell: " -f Red -nonewline;
 Write-Host "Powershell 3.0"
 
 # Line 7 - Resolution
-Write-Host ":::::::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
-Write-Host "Resolution: " -f Red -NoNewline; 
+Write-Host "      ;;;;;;;;;;; ;;;;;;;;;;;;;;;;    " -f Cyan -NoNewline;
+Write-Host "Resolution: " -f Red -NoNewline;
 Write-Host $Horizontal "x" $Vertical;
 
 # Line 8 - Windows Manager (HARDCODED, sorry bbzero users)
-Write-Host ":::::::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
-Write-Host "Window Manager: " -f Red -nonewline; 
+Write-Host "      ;;;;;;;;;;; ;;;;;;;;;;;;;;;;    " -f Cyan -NoNewline;
+Write-Host "Window Manager: " -f Red -nonewline;
 Write-Host "DWM";
 
 # Line 10 - Font (HARDCODED)
-Write-Host "................ ................       " -f Cyan -NoNewline;
-Write-Host "Font: " -f Red -nonewline; 
+Write-Host "      ;;;;;;;;;;; ;;;;;;;;;;;;;;;;    " -f Cyan -NoNewline;
+Write-Host "Font: " -f Red -nonewline;
 Write-Host "Segoe UI";
 
 # Line 11 - CPU
-Write-Host ":::::::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
-Write-Host "CPU: " -f Red -nonewline; 
+Write-Host "                                      " -f Cyan -NoNewline;
+Write-Host "CPU: " -f Red -nonewline;
 Write-Host $CPU;
 
 # Line 12 - GPU
-Write-Host ":::::::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
-Write-Host "GPU: " -f Red -nonewline; 
+Write-Host "      ;;;;;;;;;;; ;;;;;;;;;;;;;;;;    " -f Cyan -NoNewline;
+Write-Host "GPU: " -f Red -nonewline;
 Write-Host $GPU;
 
 # Line 13 - Ram
-Write-Host ":::::::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
+Write-Host "      ;;;;;;;;;;; ;;;;;;;;;;;;;;;;    " -f Cyan -NoNewline;
 Write-Host "RAM: " -f Red -nonewline;
 Write-Host $FreeRam "MB / $TotalRam MB" -NoNewline;
 Write-Host " (" -NoNewline
@@ -107,16 +108,18 @@ Write-Host $RamPercent"%" -f Green -NoNewline;
 Write-Host ")";
 
 # Line 13 - Disk Usage
-Write-Host "'''':::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
+Write-Host "      ;;;;;;;;;;; ;;;;;;;;;;;;;;;;    " -f Cyan -NoNewline;
 Write-Host "Disk: " -f Red -NoNewline;
 Write-Host $FreeDiskSizeGB"GB" " / " $DiskSizeGB"GB" -NoNewline;
-Write-Host " (" -NoNewline; 
+Write-Host " (" -NoNewline;
 Write-Host $DiskPercent"%" -f Green -NoNewline;
-Write-Host ")"; 
+Write-Host ")";
 
 # Empty Lines
-Write-Host "        '''':::: ::::::::::::::::       " -f Cyan;
-Write-Host "                 ''''::::::::::::       " -f Cyan;
-Write-Host "                         ''''::::       " -f Cyan;
-
-
+Write-Host "      ;;;;;;;;;;; ;;;;;;;;;;;;;;;;    " -f Cyan;
+Write-Host "       ;;;;;;;;;; ;;;;;;;;;;;;;;;;    " -f Cyan;
+Write-Host "         .;;;;;;; ;;;;;;;;;;;;;;;;    " -f Cyan;
+Write-Host "                . ;;;;;;;;;;;;;;;;    " -f Cyan;
+Write-Host "                        `;;;;;;;;;;    " -f Cyan;
+Write-Host "                              `:;;;    " -f Cyan;
+Write-Host `n`n
